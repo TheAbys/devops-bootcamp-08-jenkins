6b0d0403707089e08ac09d9c952c9a6387a8a92e
Sebastian Müller <sebastian.mueller@syskron.com>
2024-01-10 12:09:06 +0100

    changed to ecr

--- script.groovy
+++ script.groovy
@@ -16,10 +16,10 @@ def buildJar() {
 }
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: "nexus-docker-repo", passwordVariable: "PASS", usernameVariable: "USER")]){
-        sh 'docker build -t 64.226.110.153:8083/java-maven-app:1.2 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin 64.226.110.153:8083'
-        sh 'docker push 64.226.110.153:8083/java-maven-app:1.2'
+    withCredentials([aws(credentialsId: "aws-credentials")]){
+        sh 'docker build -t 561656302811.dkr.ecr.eu-central-1.amazonaws.com/k0938261-training:latest .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 561656302811.dkr.ecr.eu-central-1.amazonaws.com'
+        sh 'docker push 561656302811.dkr.ecr.eu-central-1.amazonaws.com/k0938261-training:latest'
     }
 }
 return this
